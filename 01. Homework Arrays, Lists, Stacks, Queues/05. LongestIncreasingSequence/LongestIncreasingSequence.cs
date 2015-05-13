6c78581691a333d642bda57e99fbd3f394dbaa39
naskobogdanov <nasko.bogdanov@gmail.com>
2015-05-13 18:14:02 +0300

    Minor Changes

---
+++ 01. Homework Arrays, Lists, Stacks, Queues/05. LongestIncreasingSequence/LongestIncreasingSequence.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LongestIncreasingSequence
+{
+    static void Main()
+    {
+        int[] intNums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        List<List<int>> intList = new List<List<int>>();
+        List<int> sublist = new List<int>();
+        int currentCount = 0;
+        int position = 0;
+
+        for (int i = 0; i < intNums.Length - 1; i++)
+        {                       
+            if (i == 0)
+            {
+                sublist.Add(intNums[i]);
+            }                
+            if (intNums[i] >= intNums[i + 1])
+            {
+                intList.Add(sublist);
+                sublist = new List<int>();
+                sublist.Add(intNums[i + 1]);              
+            }
+            else
+            {
+                sublist.Add(intNums[i + 1]);
+            }
+        }
+        intList.Add(sublist);
+        Console.WriteLine(); 
+       
+        for (int i = 0; i < intList.Count; i++)
+        {
+            for (int j = 0; j < intList[i].Count; j++)
+			{                
+			    Console.Write(intList[i][j]);
+                Console.Write(' ');
+			}
+            Console.WriteLine();
+            if (intList[i].Count > currentCount)
+            {
+                currentCount = intList[i].Count;
+                position = i;
+            }
+        }            
+        
+        Console.Write("Longest: ");
+        foreach (var item in intList[position])
+        {
+            Console.Write("{0} ", item);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 02. Homework - Methods/06. NumberCalculations/NumberCalculations.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+
+class NumberCalculations
+{
+    static decimal Minimum(decimal[] nums)
+    {
+        int position = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    position = i;
+                    break;
+                }
+                break;
+            }                 
+        }
+        return nums[position];
+    }
+
+    static double Minimum(double[] nums)
+    {
+        int position = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    position = i;
+                    break;
+                }
+                break;
+            }
+        }
+        return nums[position];
+    }
+
+    static decimal Maximum(decimal[] nums)
+    {
+        int position = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    position = i;
+                    break;
+                }
+                break;
+            }
+        }
+        return nums[position];
+    }
+
+    static double Maximum(double[] nums)
+    {
+        int position = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    position = i;
+                    break;
+                }
+                break;
+            }
+        }
+        return nums[position];
+    }
+
+    static decimal Average(decimal[] nums)
+    {
+        decimal sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        return sum/nums.Length;
+    }
+
+    static double Average(double[] nums)
+    {
+        double sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        return sum / nums.Length;
+    }
+
+    static decimal Sum(decimal[] nums)
+    {
+        decimal sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    static double Sum(double[] nums)
+    {
+        double sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    static decimal Product(decimal[] nums)
+    {
+        decimal product = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            product *= nums[i];
+        }
+        return product;
+    }
+
+    static double Product(double[] nums)
+    {
+        double product = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            product *= nums[i];
+        }
+        return product;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Please enter your numbers on a single line separated by space: ");
+        decimal[] nums = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+        //If you want to use is with doubles, just remove the comment below and put a comment on the row above
+        //double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        Console.WriteLine("Min: {0}", Minimum(nums));
+        Console.WriteLine("Max: {0}", Maximum(nums));
+        Console.WriteLine("Average: {0:F2}", Average(nums));
+        Console.WriteLine("Sum: {0}", Sum(nums));
+        Console.WriteLine("Product: {0}", Product(nums));
+    }
+}
