7a3144b19a52f98de27a316406a440822358ef73
naskobogdanov <nasko.bogdanov@gmail.com>
2015-05-14 18:21:16 +0300

    Added a new homework!

---
+++ 04. Strings and Text Processing/01. ReverseString/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ReverseString
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+        char[] ch = str.ToCharArray();
+        Array.Reverse(ch);
+        string reverserStr = new string(ch);
+        Console.WriteLine();
+        Console.WriteLine("Your reversed string is: {0}", reverserStr);
+    }
+}
